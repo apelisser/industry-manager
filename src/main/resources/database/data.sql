de5f767cd869b012868d0d1b1d661a0a63ba557a
Abner J Pelisser <abner.pelisser@gmail.com>
2024-07-06 21:06:32 -0300

    chore: add cities in sql script

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,9 +1,14 @@
-INSERT INTO COUNTRY (ID, NAME, ABBREV_NAME) VALUES (1, 'Brasil', 'BR');
-INSERT INTO COUNTRY (ID, NAME, ABBREV_NAME) VALUES (2, 'Portugal', 'PT');
-INSERT INTO COUNTRY (ID, NAME, ABBREV_NAME) VALUES (3, 'Estados Unidos', 'EUA');
-INSERT INTO COUNTRY (ID, NAME, ABBREV_NAME) VALUES (4, 'França', 'FR');
+insert into COUNTRY (ID, NAME, ABBREV_NAME) values (1, 'Brasil', 'BR');
+insert into COUNTRY (ID, NAME, ABBREV_NAME) values (2, 'Portugal', 'PT');
+insert into COUNTRY (ID, NAME, ABBREV_NAME) values (3, 'Estados Unidos', 'EUA');
+insert into COUNTRY (ID, NAME, ABBREV_NAME) values (4, 'França', 'FR');
 
-INSERT INTO STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) VALUES (1, 'Paraná', 'PR', 1);
-INSERT INTO STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) VALUES (2, 'São Paulo', 'SP', 1);
-INSERT INTO STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) VALUES (3, 'Santa Catarina', 'SC', 1);
-INSERT INTO STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) VALUES (4, 'Goiás', 'GO', 1);
+insert into STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) values (1, 'Paraná', 'PR', 1);
+insert into STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) values (2, 'São Paulo', 'SP', 1);
+insert into STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) values (3, 'Santa Catarina', 'SC', 1);
+insert into STATE (ID, NAME, ABBREV_NAME, COUNTRY_ID) values (4, 'Goiás', 'GO', 1);
+
+insert into CITY (ID, NAME, STATE_ID) values (1, 'Cruzeiro do Sul', 1);
+insert into CITY (ID, NAME, STATE_ID) values (2, 'Paranavaí', 1);
+insert into CITY (ID, NAME, STATE_ID) values (3, 'Presidente Prudente', 2);
+insert into CITY (ID, NAME, STATE_ID) values (4, 'São Paulo', 2);
