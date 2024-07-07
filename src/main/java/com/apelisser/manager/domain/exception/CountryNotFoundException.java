59fd9fe76e8273a1e3258ddfb57e3d6e64072c4a
Abner J Pelisser <abner.pelisser@gmail.com>
2024-07-06 23:00:42 -0300

    style: spaces

--- src/main/java/com/apelisser/manager/domain/exception/CountryNotFoundException.java
+++ src/main/java/com/apelisser/manager/domain/exception/CountryNotFoundException.java
@@ -3,17 +3,17 @@ package com.apelisser.manager.domain.exception;
 import java.io.Serial;
 
 public class CountryNotFoundException extends IndustryManagerException {
-    
+
     @Serial
     private static final long serialVersionUID = 8035763556392510944L;
 
     private final Long id;
-    
+
     public CountryNotFoundException(Long id) {
         this.id = id;
     }
 
-    public CountryNotFoundException(Long id,String message) {
+    public CountryNotFoundException(Long id, String message) {
         super(message);
         this.id = id;
     }
