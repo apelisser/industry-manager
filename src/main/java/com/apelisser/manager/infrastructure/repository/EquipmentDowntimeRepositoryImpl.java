78f65d1c251e51727a68be959980563391608ae4
Abner J Pelisser <abner.pelisser@gmail.com>
2024-12-08 03:12:54 -0300

    fix: avoid exception when no record found

--- src/main/java/com/apelisser/manager/infrastructure/repository/EquipmentDowntimeRepositoryImpl.java
+++ src/main/java/com/apelisser/manager/infrastructure/repository/EquipmentDowntimeRepositoryImpl.java
@@ -29,7 +29,11 @@ public class EquipmentDowntimeRepositoryImpl implements EquipmentDowntimeReposit
 
         TypedQuery<EquipmentDowntime> query = manager.createQuery(criteria);
         query.setMaxResults(1);
-        return Optional.ofNullable(query.getSingleResult());
+
+        List<EquipmentDowntime> resultList = query.getResultList();
+        return resultList.isEmpty() ?
+            Optional.empty() :
+            Optional.ofNullable(resultList.getFirst());
     }
 
     @Override
@@ -45,41 +49,45 @@ public class EquipmentDowntimeRepositoryImpl implements EquipmentDowntimeReposit
 
         CriteriaQuery<EquipmentDowntime> criteria = builder.createQuery(EquipmentDowntime.class);
         Root<EquipmentDowntime> root = criteria.from(EquipmentDowntime.class);
+        List<Predicate> predicates = new ArrayList<>();
 
+        // Define predicates for overlapping downtimes based on start and end times
         Predicate startTimeIsBeforeNewEndTime = builder.lessThan(root.get("startTime"), equipmentDowntime.getEndTime());
         Predicate endTimeIsAfterNewStartTime = builder.greaterThan(root.get("endTime"), equipmentDowntime.getStartTime());
 
-        List<Predicate> predicates = new ArrayList<>();
-
+        // Add predicate to ensure the equipment matches
         Predicate isSameEquipment = builder.equal(root.get("equipment"), equipmentDowntime.getEquipment());
         predicates.add(isSameEquipment);
 
+        // Exclude the current record if it is an update
         if (isAnUpdateRecord(equipmentDowntime)) {
-            // OK :: se for update: desconsiderar a si proprio na consulta
             Predicate notId = builder.notEqual(root.get("id"), equipmentDowntime.getId());
             predicates.add(notId);
         }
 
+        // Check for overlapping times depending on the presence of start and end times
         if (startAndEndTimeExists(equipmentDowntime)) {
-            // OK :: se existir inicio e fim no registro a ser salvo
+            // Create predicates to check if existing events overlap with the new event times
             Predicate startTimeIsGreaterOrEqualNewStartTime = builder.greaterThanOrEqualTo(root.get("startTime"), equipmentDowntime.getStartTime());
-
             Predicate checkStartAndEndOverlaps = builder.and(startTimeIsBeforeNewEndTime, endTimeIsAfterNewStartTime);
             Predicate startTimeBetweenNewEventTime = builder.and(startTimeIsGreaterOrEqualNewStartTime, startTimeIsBeforeNewEndTime);
             Predicate finalOrPredicate = builder.or(checkStartAndEndOverlaps, startTimeBetweenNewEventTime);
+
             predicates.add(finalOrPredicate);
         } else if (onlyStartTimeExists(equipmentDowntime)) {
-            // OK :: se existir apenas o inicio no registro a ser salvo
-            Predicate startTimeEquals = builder.equal(root.get("startTime"), equipmentDowntime.getStartTime());
-
+            // Create predicates to check if new start time overlaps with existing events
+            Predicate startTimesAreTheSame = builder.equal(root.get("startTime"), equipmentDowntime.getStartTime());
             Predicate startTimeIsBeforeNewStartTime = builder.lessThan(root.get("startTime"), equipmentDowntime.getStartTime());
             Predicate newStartTimeBetweenEventTime = builder.and(endTimeIsAfterNewStartTime, startTimeIsBeforeNewStartTime);
+            Predicate finalOrPredicate = builder.or(newStartTimeBetweenEventTime, startTimesAreTheSame);
 
-            Predicate finalOrPredicate = builder.or(newStartTimeBetweenEventTime, startTimeEquals);
             predicates.add(finalOrPredicate);
         }
 
+        // Apply the predicates to the criteria query
         criteria.where(predicates.toArray(new Predicate[0]));
+
+        // Specify ordering of results by start time
         criteria.orderBy(builder.asc(root.get("startTime")));
 
         return criteria;
