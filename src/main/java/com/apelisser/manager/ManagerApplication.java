146504bcf3f2510c54a92ddd5dc9ed13034640fc
Abner J Pelisser <abner.pelisser@gmail.com>
2025-01-01 15:53:20 -0300

    chore: set utc timezone as default

--- src/main/java/com/apelisser/manager/ManagerApplication.java
+++ src/main/java/com/apelisser/manager/ManagerApplication.java
@@ -7,12 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableConfigurationProperties({ I18NProperties.class })
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class ManagerApplication {
 
 	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 
